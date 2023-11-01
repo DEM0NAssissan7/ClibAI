14aa70e308a2820c420d4bfb85e35d70571554c2
Abdurahman Elmawi <42104518+DEM0NAssissan7@users.noreply.github.com>
2023-10-31 22:22:38 -0500

    Add files via upload

---
+++ libai.c
@@ -0,0 +1,65 @@
+#include "libai.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+float input[2];
+float target[1];
+float inputs[4][2] = {
+    {0, 0},
+    {1, 0},
+    {0, 1},
+    {1, 1}
+};
+float targets[4][1] = {
+    {0},
+    {1},
+    {1},
+    {0}
+};
+
+long trials = 0;
+
+Network network;
+Layout layout;
+
+void r_array(int len, float *a, float *b){
+    for(int i = 0; i < len; i++)
+        a[i] = b[i];
+}
+void set_io(){
+    int index = trials % 4;
+    r_array(2, input, inputs[index]);
+    r_array(2, target, targets[index]);
+    trials++;
+}
+
+
+void train(){
+    set_io();
+    nettrain(&network, input, target);
+}
+void test(){
+    for(int i = 0; i < 4; i++){
+        set_io();
+        netrun(&network, input);
+        netprintout(&network);
+    }
+}
+
+int main(){
+    layout = netlay(2, 2, 1);
+    network = netinit(&layout, 4, 0.1);
+    
+    printf("Before training:\n");
+    test();
+    
+    for(int i = 0; i < 1000000; i++)
+        train();
+
+
+    printf("\nAfter training:\n");
+    netdebug(&network);
+    test();
+    return 0;
+}
